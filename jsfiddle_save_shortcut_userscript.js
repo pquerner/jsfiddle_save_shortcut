9fb3a5a1e52efdfe5222be99dccb7fde08faf6c3
Pascal Querner <pascal.querner@mscg.de>
2015-01-22 09:07:17 +0100

    Added js file Saves new files just fine but ignores update as of now. (Instead it creates a new script (new uri))

---
+++ jsfiddle_save_shortcut_userscript.js
@@ -0,0 +1,26 @@
+/**
+ * Created by p.querner on 22.01.2015.
+ */
+// ==UserScript==
+// @name       My Fancy New Userscript
+// @namespace  https://github.com/pquerner
+// @version    0.1
+// @description  enter something useful
+// @match      http://*.jsfiddle.net/*/*
+// @match      http://*.jsfiddle.net
+// @copyright  2015, Me
+// @updateURL https://github.com/pquerner/jsfiddle_save_shortcut/jsfiddle_save_shortcut_userscript.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/mousetrap/1.4.6/mousetrap.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/mousetrap/1.4.6/mousetrap-global-bind.min.js
+// ==/UserScript==
+
+Mousetrap.bindGlobal(['command+s', 'ctrl+s'], function() {
+    if(document.getElementById('update') != undefined) { //Update found
+        document.getElementById('update').click(); //Emulate click
+    }
+    if(document.getElementById('savenew') != undefined) { //Savenew found
+        document.getElementById('savenew').click(); //Emulate click
+    }
+    // return false to prevent default browser behavior
+    return false;
+});
