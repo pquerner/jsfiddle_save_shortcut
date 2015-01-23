20623be54568d69450f653154768da1d47517f10
Pascal Querner <pascal.querner@mscg.de>
2015-01-23 08:18:59 +0100

    Bump version

--- jsfiddle_save_shortcut_userscript.js
+++ jsfiddle_save_shortcut_userscript.js
@@ -5,7 +5,7 @@
 // ==UserScript==
 // @name       jsfiddle Save/Update Shortcut Fix
 // @namespace  https://github.com/pquerner
-// @version    0.1
+// @version    0.2
 // @description  Fixes jsfiddle error on Chrome when trying to save the current fiddle (either update or new fiddles)
 // @match      http://*.jsfiddle.net/*/*
 // @match      http://*.jsfiddle.net
